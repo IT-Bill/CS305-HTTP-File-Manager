25db5b220b04edf82d370a224306161b263f819b
Bill <704317542@qq.com>
2023-12-22 22:53:25 +0800

    Update chunked transfer

--- lib/server/HTTPRequestHandler.py
+++ lib/server/HTTPRequestHandler.py
@@ -38,6 +38,199 @@ class HTTPRequestHandler:
         self._request = Request()
         self._response = Response(self.wfile)
 
+    def handle(self):
+        """Handle the http request"""
+        self.close_connection = False
+
+        self.handle_one_request()
+        while not self.close_connection:
+            self.handle_one_request()
+
+    def handle_one_request(self):
+        """Handle a single HTTP request"""
+        # `readline` is used to read one line of request message
+        start_line = str(self.rfile.readline(1024), "iso-8859-1").rstrip("\r\n")
+        print(start_line)
+        # GET /path HTTP/1.1
+        command, path, _ = start_line.split()
+        if path.startswith("//"):
+            path = "/" + path.lstrip("/")
+        if path.endswith("//"):
+            path = path.rstrip("/") + "/"
+
+        self._request.cmd, self._request.path = command, path
+        self._request.simple_path, self._request.query = utils.parse_url(self._request.path)
+        # parse header
+        self._request.headers = HTTPMessage.parse_headers(self.rfile)
+
+        # invoke the corresponding method
+        method = getattr(self, f"do_{self._request.cmd}")
+        method()
+        # actually send the response
+        self.wfile.flush()
+    
+    def send_chunked_response(self, f):
+        """Send data in chunks for chunked transfer encoding."""
+        while True:
+            chunk = f.read(8192)
+            if not chunk:
+                break
+                
+            self.wfile.write(f"{len(chunk):X}\r\n".encode())
+            self.wfile.write(chunk)
+            self.wfile.write(b"\r\n")
+        
+        self.wfile.write(b"0\r\n\r\n")
+
+
+    def do_GET(self):
+        """Serve a GET request"""
+        if self.is_unauthorized() or self.is_forbidden() or self.is_bad_request():
+            return
+        else:
+            f = self.send_head()
+            if f:
+                try:
+                    # chunked transfer
+                    if self._request.query.get('chunked') == ['1']:
+                        self._response.add_header('Transfer-Encoding', 'chunked')
+                        self._response.remove_header('Content-Length')
+                        self._response.write_headers()
+                        self.send_chunked_response(f)
+                    else:
+                        self._response.write_headers()
+                        shutil.copyfileobj(f, self.wfile)
+                finally:
+                    f.close()
+
+    def do_POST(self):
+        """ Serve a POST request """
+        if self.is_unauthorized() or self.is_bad_request() or self.is_forbidden():
+            return
+        
+        if self.post_cmd == "upload":
+            self.upload()
+        elif self.post_cmd == "delete":
+            self.delete()
+
+    def do_HEAD(self):
+        """Serve a HEAD request"""
+        f = self.send_head()
+        if f:
+            f.close()
+
+    def send_head(self):
+        path = self.path2local(self._request.path)
+        f = None
+        if os.path.isdir(path):
+            parts = urllib.parse.urlsplit(self._request.path)
+            if not parts.path.endswith("/"):
+                # Example: Redirect `/dir` to `/dir/`
+                new_parts = (parts[0], parts[1], parts[2] + "/", parts[3], parts[4])
+                new_url = urllib.parse.urlunsplit(new_parts)
+                self.redirect(new_url, HTTPStatus.PERMANENT_REDIRECT)
+                return None
+            for index in "index.html", "index.htm":
+                index = os.path.join(path, index)
+                if os.path.exists(index):
+                    path = index
+                    break
+            else:
+                return self.list_directory(path)
+            
+        ctype, _ = mimetypes.guess_type(path)
+
+        # parseing and rejection of filenames with a trailing slash
+        if path.endswith("/"):
+            self.response_error(HTTPStatus.NOT_FOUND)
+            return None
+        
+        try:
+            if self._request.path == "/favicon.ico":
+                f = open(os.path.join(os.getcwd(), "favicon.ico"), "rb")
+            else:
+                f = open(path, "rb")
+        except OSError:
+            self.response_error(HTTPStatus.NOT_FOUND)
+            return None
+
+        try:
+            fs = os.fstat(f.fileno())
+            # TODO: use browser cache
+
+            self._response.set_status_line(HTTPStatus.OK)
+            self._response.add_header("Content-type", ctype)
+            self._response.add_header("Content-Length", fs.st_size)
+            self._response.add_header("Last-Modified", utils.formatdate(fs.st_mtime))
+            # self._response.write_headers()
+            return f
+        except:
+            f.close()
+            raise
+
+    def delete(self):
+        path = self.path2local(self._request.query["path"][0])
+        if not os.path.isfile(path):
+            self.response_error(HTTPStatus.NOT_FOUND)
+            return
+        try:
+            os.remove(path)
+            self._response.set_status_line(HTTPStatus.OK)
+            self._response.write_headers()
+            
+        except OSError:
+            self.response_error(HTTPStatus.INTERNAL_SERVER_ERROR)
+
+    def upload(self):
+        path = self.path2local(self._request.query["path"][0])
+        
+        if not os.path.isdir(path):
+            # the directory does not exist
+            self.response_error(HTTPStatus.NOT_FOUND)
+            return
+        
+        content_length = int(self._request.get_header('Content-Length'))
+        file_data = self.rfile.read(content_length)
+        file_name = utils.get_filename_from_content_disposition(self._request.get_header('Content-Disposition'))
+        if file_name is None:
+            file_name = str(uuid.uuid1())
+        file_path = os.path.join(path, file_name)
+        with open(file_path, 'wb') as file:
+            file.write(file_data)
+        
+        self._response.set_status_line(HTTPStatus.OK)
+        self._response.write_headers()
+
+    
+
+    def is_bad_request(self):
+        if self._request.cmd == "GET":
+            # check SUSTech-HTTP query
+            # not dir
+            if os.path.isfile(self.path2local(self._request.path)) or \
+                self._request.query.get("SUSTech-HTTP") and \
+                self._request.query["SUSTech-HTTP"] in (["0"], ["1"]):
+                return False # correct
+            
+        elif self._request.cmd == "POST":
+            segments = [
+                seg
+                for seg in posixpath.normpath(
+                    urllib.parse.unquote(self._request.simple_path)
+                ).split("/")
+                if seg
+            ]
+            if len(segments) == 1 and segments[0] in ["upload", "delete"]:
+                if self._request.query.get("path") != None and \
+                    len(self._request.query["path"]) == 1:
+
+                    # Recode the post command
+                    self.post_cmd = segments[0]
+                    return False
+        
+        self.response_error(HTTPStatus.BAD_REQUEST)
+        return True
+
     def is_unauthorized(self):
         """
         Varify the authorization.
@@ -111,189 +304,12 @@ class HTTPRequestHandler:
         self.response_error(HTTPStatus.FORBIDDEN)    
         return True
 
-    def handle(self):
-        """Handle the http request"""
-        self.close_connection = False
-
-        self.handle_one_request()
-        while not self.close_connection:
-            self.handle_one_request()
-
-    def handle_one_request(self):
-        """Handle a single HTTP request"""
-        # `readline` is used to read one line of request message
-        start_line = str(self.rfile.readline(1024), "iso-8859-1").rstrip("\r\n")
-        print(start_line)
-        # GET /path HTTP/1.1
-        command, path, _ = start_line.split()
-        if path.startswith("//"):
-            path = "/" + path.lstrip("/")
-        if path.endswith("//"):
-            path = path.rstrip("/") + "/"
-
-        self._request.cmd, self._request.path = command, path
-        self._request.simple_path, self._request.query = utils.parse_url(self._request.path)
-        # parse header
-        self._request.headers = HTTPMessage.parse_headers(self.rfile)
-
-        # invoke the corresponding method
-        method = getattr(self, f"do_{self._request.cmd}")
-        method()
-        # actually send the response
-        self.wfile.flush()
-        
-        
-    
-    def do_GET(self):
-        """Serve a GET request"""
-        if self.is_unauthorized() or self.is_forbidden() or self.is_bad_request():
-            return
-        else:
-            f = self.send_head()
-            if f:
-                try:
-                    shutil.copyfileobj(f, self.wfile)
-                finally:
-                    f.close()
-
-    def do_POST(self):
-        """ Serve a POST request """
-        if self.is_unauthorized() or self.is_bad_request() or self.is_forbidden():
-            return
-        
-        if self.post_cmd == "upload":
-            self.upload()
-        elif self.post_cmd == "delete":
-            self.delete()
-
-
-    def do_HEAD(self):
-        """Serve a HEAD request"""
-        f = self.send_head()
-        if f:
-            f.close()
-    
-    def delete(self):
-        path = self.path2local(self._request.query["path"][0])
-        if not os.path.isfile(path):
-            self.response_error(HTTPStatus.NOT_FOUND)
-            return
-        try:
-            os.remove(path)
-            self._response.set_status_line(HTTPStatus.OK)
-            self._response.write_headers()
-            
-        except OSError:
-            self.response_error(HTTPStatus.INTERNAL_SERVER_ERROR)
-    
-    def upload(self):
-        path = self.path2local(self._request.query["path"][0])
-        
-        if not os.path.isdir(path):
-            # the directory does not exist
-            self.response_error(HTTPStatus.NOT_FOUND)
-            return
-        
-        content_length = int(self._request.get_header('Content-Length'))
-        file_data = self.rfile.read(content_length)
-        file_name = utils.get_filename_from_content_disposition(self._request.get_header('Content-Disposition'))
-        if file_name is None:
-            file_name = str(uuid.uuid1())
-        file_path = os.path.join(path, file_name)
-        with open(file_path, 'wb') as file:
-            file.write(file_data)
-        
-        self._response.set_status_line(HTTPStatus.OK)
-        self._response.write_headers()        
-
-    def is_bad_request(self):
-        if self._request.cmd == "GET":
-            # check SUSTech-HTTP query
-            # not dir
-            if not os.path.isdir(self.path2local(self._request.path)) or \
-                len(self._request.query) == 1 and \
-                self._request.query.get("SUSTech-HTTP") != None and \
-                len(self._request.query["SUSTech-HTTP"]) == 1 and \
-                self._request.query["SUSTech-HTTP"][0] in ("0", "1"):
-                return False  # correct
-            
-        elif self._request.cmd == "POST":
-            segments = [
-                seg
-                for seg in posixpath.normpath(
-                    urllib.parse.unquote(self._request.simple_path)
-                ).split("/")
-                if seg
-            ]
-            if len(segments) == 1 and segments[0] in ["upload", "delete"]:
-                if len(self._request.query) == 1 and \
-                    self._request.query.get("path") != None and \
-                    len(self._request.query["path"]) == 1:
-
-                    # Recode the post command
-                    self.post_cmd = segments[0]
-                    return False
-        
-        self.response_error(HTTPStatus.BAD_REQUEST)
-        return True
-
-
     def redirect(self, new_url, status=HTTPStatus.TEMPORARY_REDIRECT):
         """Redirect to new url."""
         self._response.set_status_line(status)
         self._response.add_header("Location", new_url)
         self._response.add_header("Content-Length", "0")
         self._response.write_headers()
-
-    def send_head(self):
-        path = self.path2local(self._request.path)
-        f = None
-        if os.path.isdir(path):
-            parts = urllib.parse.urlsplit(self._request.path)
-            if not parts.path.endswith("/"):
-                # Example: Redirect `/dir` to `/dir/`
-                new_parts = (parts[0], parts[1], parts[2] + "/", parts[3], parts[4])
-                new_url = urllib.parse.urlunsplit(new_parts)
-                self.redirect(new_url, HTTPStatus.PERMANENT_REDIRECT)
-                return None
-            for index in "index.html", "index.htm":
-                index = os.path.join(path, index)
-                if os.path.exists(index):
-                    path = index
-                    break
-            else:
-                return self.list_directory(path)
-            
-        ctype, _ = mimetypes.guess_type(path)
-
-        # parseing and rejection of filenames with a trailing slash
-        if path.endswith("/"):
-            self.response_error(HTTPStatus.NOT_FOUND)
-            return None
-        
-        try:
-            if self._request.path == "/favicon.ico":
-                f = open(os.path.join(os.getcwd(), "favicon.ico"), "rb")
-            else:
-                f = open(path, "rb")
-        except OSError:
-            self.response_error(HTTPStatus.NOT_FOUND)
-            return None
-
-        try:
-            fs = os.fstat(f.fileno())
-            # TODO: use browser cache
-
-            self._response.set_status_line(HTTPStatus.OK)
-            self._response.add_header("Content-type", ctype)
-            self._response.add_header("Content-Length", fs.st_size)
-            self._response.add_header("Last-Modified", utils.formatdate(fs.st_mtime))
-            self._response.write_headers()
-            return f
-        except:
-            f.close()
-            raise
-
 
     def list_directory(self, path):
         """Helper to produce a directory listing (absent index.html).
@@ -373,7 +389,7 @@ class HTTPRequestHandler:
         self._response.set_status_line(HTTPStatus.OK)
         self._response.add_header("Content-type", "text/html; charset=%s" % enc)
         self._response.add_header("Content-Length", str(len(encoded)))
-        self._response.write_headers()
+        # self._response.write_headers()
         return f
 
     def path2local(self, path):
